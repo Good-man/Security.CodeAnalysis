373a5c28653b68cea19db256562daf74833e04fc
Mark A Good <mark.alan.good@gmail.com>
2015-05-25 08:35:37 -0400

    add solution file add test project add git ignore add git attributes

---
+++ Security.CodeAnalysis.Test/CsrfController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityWeaknesses.Web.Controllers
+{
+    public class CsrfController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Default
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            return View();
+        }
+
+        // POST: Default
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult PostWithoutAntiForgery(FormCollection formCollection)
+        {
+            return View();
+        }
+
+        internal void SomeOtherMethod()
+        {
+            // do something
+        }
+    }
+}

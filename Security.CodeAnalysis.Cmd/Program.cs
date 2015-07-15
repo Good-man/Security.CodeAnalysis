ea8c7bd7605b45012be255f836557b8de514b23b
Mark A Good <mark.alan.good@gmail.com>
2015-07-15 12:35:00 -0400

    command line parser

--- Security.CodeAnalysis.Cmd/Program.cs
+++ Security.CodeAnalysis.Cmd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using Fclp;
 
 namespace Security.CodeAnalysis.Cmd
 {
@@ -7,14 +8,24 @@ namespace Security.CodeAnalysis.Cmd
     {
         private static void Main(string[] args)
         {
-            var fileName = args[0];
-            if (!File.Exists(fileName))
-                throw new FileNotFoundException("File not found", fileName);
+            var p = new FluentCommandLineParser();
 
-            var checker = new DotNetChecker(fileName);
+            p.Setup<string>('t', "target")
+                .Callback(filename => FileName = filename)
+                .Required()
+                .WithDescription("Full path to the target project, solution or file.");
+
+            p.Parse(args);
+
+            if (!File.Exists(FileName))
+                throw new FileNotFoundException("File not found", FileName);
+
+            var checker = new DotNetChecker(FileName);
             checker.CodeIssueFound += checker_CodeIssueFound;
             checker.Check();
         }
+
+        public static string FileName { get; set; }
 
         private static void checker_CodeIssueFound(object sender, CodeIssueFoundEventArgs e)
         {
